c14e399737363dd6067001d61455aaaa5a7ff5ae
Michael Shih <sc2.shih@gmail.com>
2019-10-21 17:21:48 -0500

    load images by datagen flow from directory

--- ML/2Dmod/main_notebook_test.py
+++ ML/2Dmod/main_notebook_test.py
@@ -1,6 +1,7 @@
 
 #%%
 # import os, glob, shutil
+import os, sys
 import cv2
 import numpy as np
 import uuid
@@ -21,6 +22,43 @@ path = "/Volumes/LaCie_DataStorage/PerlmutterData/training/cell_membrane/prepdat
 imgpath = list(paths.list_images(path))
 print(imgpath[0])
 
+#%%
+data_gen_args = dict(
+                featurewise_center=True,
+                featurewise_std_normalization=True,
+                horizontal_flip=True,
+                vertical_flip=True,
+                rotation_range=90.,
+                width_shift_range=0.1,
+                height_shift_range=0.1,
+                shear_range=0.07,
+                zoom_range=0.2,
+                fill_mode='constant',
+                cval=0.,)
 
+seed = 100
+
+#%%
+image_datagen = ImageDataGenerator(**data_gen_args)
+label_datagen = ImageDataGenerator(**data_gen_args)
+
+#%%
+inputimage = os.path.join(path, 'images')
+print(inputimage)
+
+#%%
+image_generator = image_datagen.flow_from_directory(
+    os.path.join(path, 'data/images/'),
+    class_mode=None,
+    seed=seed)
+
+label_generator = label_datagen.flow_from_directory(
+    os.path.join(path, 'data/labels'),
+    class_mode=None,
+    seed=seed)
+
+#%%
+image_datagen.fit(images, augment=True, seed=seed) 
+mask_datagen.fit(masks, augment=True, seed=seed)
 
 #%%
